485fb2d1c8c6ba54c0ae2b562bddbdd795aa30d4
bammyu <mbjun3630@naver.com>
2025-04-13 10:58:56 +0900

    Push 함수 사용

--- lib/screens/3_push_screen.dart
+++ lib/screens/3_push_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_lv2_go_router/layout/default_layout.dart';
 import 'package:go_router/go_router.dart';
 
-class RootScreen extends StatelessWidget {
-  const RootScreen({super.key});
+class PushScreen extends StatelessWidget {
+  const PushScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,22 +12,20 @@ class RootScreen extends StatelessWidget {
         children: [
           ElevatedButton(
             onPressed: () {
+              // stack
+              // push -> basic -> push
+              context.push('/basic');
+            },
+            child: const Text('Push Basic'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // stack
+              // push -> basic -> root
               context.go('/basic');
             },
-            child: const Text('Go BasicScreen'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.goNamed('named_screen');
-            },
-            child: const Text('Go NamedScreen'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go('/push');
-            },
-            child: const Text('Go PushScreen'),
-          ),
+            child: const Text('Go Basic'),
+          )
         ],
       ),
     );
